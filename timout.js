6a282be4b10340c590458232158a45c4c68624ae
Bharat <bharat.sinha.2307@gmail.com>
2017-06-02 15:09:01 +0530

    Change Code to Objectt orientated JS

--- timout.js
+++ timout.js
@@ -1,30 +1,44 @@
-//Initialize the Count or Time in sec equals to zero
-var count = 0;
-
+var TimeoutJS = function(HTML,counter,id){
+    //Initialize the Count or Time in sec equals to zero
+    this.count = 0;
+    this.countLimit = counter;
+    var that = this;
 //Assign SetInterval to a variable
-PageTime = setInterval(function() {
-    //Increament Count on Each Interval Call i.e. Each Second
-    count += 1;
-    //If Time is Greater than 20 sec Load the Timeout Page : Change time as per need
-    if (count === 20) {
-	//code to clear the interval
-        clearInterval(pageTime);
-	//Insert the HTML here which will be painted after timeout is encountered
-        document.write("Code FOr Page To Be Displayed Here");
-    }
-}, 1000);
+    this.pageTime = setInterval(function() {
+        //Increament Count on Each Interval Call i.e. Each Second
+        that.count += 1;
+        //If Time is Greater than 20 sec Load the Timeout Page : Change time as per need
+        if (that.count >= that.countLimit) {
+        //code to clear the interval
+            clearInterval(that.pageTime);
+        //Insert the HTML here which will be painted after timeout is encountered
+            document.write(HTML);
+        }
+    }, 1000);
 
-//This determines the state of the entire document
-document.onreadystatechange = function() {
-    //Here we selected Interactive event to encounter the Timeout 
-    //Load Time or DOM content loaded time can also be used
-    if (document.readyState === 'interactive') {
-        clearInterval(pageTime);
+    //This determines the state of the entire document
+    document.onreadystatechange = function() {
+        //Here we selected Interactive event to encounter the Timeout 
+        //Load Time or DOM content loaded time can also be used
+        if (document.readyState === 'load') {
+            console.log(that.countLimit);
+            clearInterval(that.pageTime);
+        }
     }
-}
 
-//Function to reload the page can be call using the HTML inserted by document.write
-//Can be call on an onclick event of a buttom
-function reloadPage() {
-    location.reload();
+    if(id){
+        var elem = document.getElementById(id);
+        elem.onclick = function(){
+            if(callback){
+                callback();
+            }            
+            that.reloadPage();
+        };
+    }
+
+    //Function to reload the page can be call using the HTML inserted by document.write
+    //Can be call on an onclick event of a buttom
+    this.reloadPage = function() {
+        location.reload();
+    }
 }
